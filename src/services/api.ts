af3102e8ef0292dfb6ed255b6b5afe0f26ce43f3
Carl Munn <me@carlmunn.com>
2025-08-22 14:19:46 +0100

    Use relative paths. Deploy to carlmunn.name
    - Thought I might also place this on AWS
    
    See https://carlmunn.name/demos/brighthr/demo-33449f.html

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 const apiGet = function(){
   console.debug('[D][apiGet] GET data.json')
 
-  return fetch('/api/v1/data.json', {method: 'GET'}).then((resp) => {
+  // The path is relative. See 'base' in index.html
+  return fetch('api/v1/data.json', {method: 'GET'}).then((resp) => {
     return resp.json()
   })
 }
